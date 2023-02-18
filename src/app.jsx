ad04ad8fdfc9c7cc8b72b78a316a1f1cedbc552e
shen <santi.dionis@gmail.com>
2023-02-18 18:00:45 +0100

    ADD: media

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,11 @@ const App = () => {
         
         <ProductContainer>
         <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
         </ProductContainer>
     
     )
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import './Product.css'
-import AddCardBtn from './AddCardBtn'
+import React from "react";
+import "./Product.css";
+import AddCardBtn from "./AddCardBtn";
 
 export const Product = () => {
   return (
-    
-    <div className='product'>
-    <img className='product__img' src='https://picsum.photos/200' alt='photo' />
-    <h2 className='product__name'>Camisa</h2>
-    <div className='product__details'>
-        <p className='product__oldPrice'>200$</p>
-        <p className='product__price'>300$</p>
-        
-
+    <div className="product">
+      <img
+        className="product__img"
+        src="https://picsum.photos/200"
+        alt="photo"
+      />
+      <h2 className="product__name">Camisa</h2>
+      <div className="product__details">
+        <p className="product__oldPrice">200$</p>
+        <p className="product__price">300$</p>
+      </div>
+      <AddCardBtn />
     </div>
-    <AddCardBtn />
-    </div>
-    )
-}
-export  default Product
+  );
+};
+export default Product;
